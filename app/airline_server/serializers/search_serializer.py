2c65ccb39144a47c410f080648516654cb981796
MiskLG <aca.hornjak@gmail.com>
2023-03-14 21:26:16 +0100

    feat: add pagination to search results
    Add pagination classes in pagination.py.
    Pagination is easy to use, see search_views.py for an example

--- app/airline_server/serializers/search_serializer.py
+++ app/airline_server/serializers/search_serializer.py
@@ -1,5 +1,3 @@
-import datetime
-
 from rest_framework import serializers
 
 from .place_serializer import PlaceSerializer
--- app/airline_server/views/search_views.py
+++ app/airline_server/views/search_views.py
@@ -3,13 +3,17 @@ from rest_framework import generics
 
 from ..filters import SearchFlightFilter
 from ..models import Flight
+from ..pagination import StandardResultsSetPagination
 from ..serializers.search_serializer import SearchFlightOutputSerializer
 
 
 class SearchList(generics.ListAPIView):
     serializer_class = SearchFlightOutputSerializer
-    queryset = Flight.objects.all()
-    serializer_class = SearchFlightOutputSerializer
-    filter_backends = [DjangoFilterBackend]
-    search_fields = ['date_of_departure', 'route__start_point__id', 'route__end_point__id', 'number_of_passengers']
+    pagination_class = StandardResultsSetPagination
     filterset_class = SearchFlightFilter
+
+    filter_backends = [DjangoFilterBackend]
+
+    queryset = Flight.objects.all()
+    search_fields = ['date_of_departure', 'route__start_point__id', 'route__end_point__id', 'number_of_passengers']
+
