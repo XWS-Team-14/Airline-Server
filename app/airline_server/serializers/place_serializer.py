cae3e079fa6f833b52b715866c5169e2e191bcd3
MiskLG <aca.hornjak@gmail.com>
2023-03-14 18:17:49 +0100

    refactor: add ids to place and route serializers
    Place and Route basic serializers now return ids of themselfs

--- app/airline_server/serializers/place_serializer.py
+++ app/airline_server/serializers/place_serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
+
 from ..models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
-        fields = ['country', 'airport_city', 'airport_name']
+        fields = ['id', 'country', 'airport_city', 'airport_name']
         read_only_fields = ['id']
--- app/airline_server/serializers/route_serializer.py
+++ app/airline_server/serializers/route_serializer.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
+
 from ..models import Route
-from .place_serializer import PlaceSerializer
 
 
 class RouteSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Route
-        fields = ['start_point', 'end_point']
+        fields = ['id', 'start_point', 'end_point']
         read_only_fields = ['id']
 
