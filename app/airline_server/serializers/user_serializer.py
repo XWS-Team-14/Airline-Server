fc5b8f8e9fb0ffd563e299d834467ab86d038508
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2023-03-15 15:12:34 +0100

    fix: update user serializer fields

--- app/airline_server/serializers/user_serializer.py
+++ app/airline_server/serializers/user_serializer.py
@@ -5,5 +5,5 @@ from ..models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'password']
-        read_only_fields = ['id', 'date_joined', 'last_login']
+        fields = ['first_name', 'last_name', 'email', 'password', 'id', 'date_joined', 'last_login', 'is_active']
+        read_only_fields = ['id', 'date_joined', 'last_login', 'is_active']
