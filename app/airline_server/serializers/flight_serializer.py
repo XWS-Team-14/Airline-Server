8b39b05bf786db48717c8b840c3e3fcbd019b3de
BogdanDavinic <davinic.ra153.2019@uns.ac.rs>
2023-03-23 18:15:36 +0100

    Changes to flight serializer to enable add functionality

--- app/airline_server/serializers/flight_serializer.py
+++ app/airline_server/serializers/flight_serializer.py
@@ -4,14 +4,18 @@ from .search_serializer import RouteOutputSerializer
 from ..models import Flight, Route
 
 
-class FlightSerializer(serializers.ModelSerializer):
-
-
-
+class FlightDisplaySerializer(serializers.ModelSerializer):
+    route = RouteOutputSerializer()
     class Meta:
         model = Flight
         fields = ['id', 'route', 'date_of_departure', 'ticket_price', 'number_of_seats', 'number_of_free_spaces']
         read_only_fields = ['id']
 
+class AddFlightSerializer(serializers.ModelSerializer):
+    route = RouteSerializer
+    class Meta:
+        model = Flight
+        fields = ['id', 'route', 'date_of_departure', 'ticket_price', 'number_of_seats', 'number_of_free_spaces']
+        read_only_fields = ['id']
 
 
--- app/airline_server/views/flight_views.py
+++ app/airline_server/views/flight_views.py
@@ -2,11 +2,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from ..models import Flight
 from ..pagination import StandardResultsSetPagination
-from ..serializers.flight_serializer import FlightSerializer
+from ..serializers.flight_serializer import FlightDisplaySerializer, AddFlightSerializer
 
 
 class FlightList(generics.ListAPIView):
-    serializer_class = FlightSerializer
+    serializer_class = FlightDisplaySerializer
     pagination_class = StandardResultsSetPagination
     filter_backends = [DjangoFilterBackend]
     queryset = Flight.objects.all()
@@ -14,16 +14,16 @@ class FlightList(generics.ListAPIView):
 
 class FlightCreateView(generics.CreateAPIView):
     queryset = Flight.objects.all()
-    serializer_class = FlightSerializer
+    serializer_class = AddFlightSerializer
 
 
 class FlightDeleteView(generics.DestroyAPIView):
     queryset = Flight.objects.all()
-    serializer_class = FlightSerializer
+    serializer_class = FlightDisplaySerializer
     lookup_field = 'id'
 
 
 class FlightUpdateView(generics.UpdateAPIView):
     queryset = Flight.objects.all()
-    serializer_class = FlightSerializer
+    serializer_class = FlightDisplaySerializer
     lookup_field = 'id'
